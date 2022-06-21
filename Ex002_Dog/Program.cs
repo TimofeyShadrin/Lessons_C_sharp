c19eba142f2286bfb39d455f182931f173f31d9e
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-21 22:35:55 +0300

    Ex 002 about dog distance fixed for test

--- Ex002_Dog/Program.cs
+++ Ex002_Dog/Program.cs
@@ -3,7 +3,7 @@ int firstFreindSpeed = 2;
 int secondFreindSpeed = 1;
 int dogSpeed = 5;
 int freind = 2;
-double distance = 50;
+double distance = 20;
 double time = 0;
 
 while (distance > 2)
@@ -15,7 +15,7 @@ while (distance > 2)
     }
 
     if (freind == 2)
-    //else
+    
     {
         time = distance / (dogSpeed + secondFreindSpeed);
         freind = 1;
