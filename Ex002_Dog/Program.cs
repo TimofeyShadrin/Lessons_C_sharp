248f0da7cdce87062c296a2c87eaad34448366be
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-21 22:25:48 +0300

    Ex 002 about dog finished

--- Ex002_Dog/Program.cs
+++ Ex002_Dog/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0;
+int firstFreindSpeed = 2;
+int secondFreindSpeed = 1;
+int dogSpeed = 5;
+int freind = 2;
+int distance = 50;
+int time = 0;
+
+while (distance > 2)
+{
+    if (freind == 1)
+    {
+        time = distance / (firstFreindSpeed + dogSpeed);
+        freind = 2; 
+    }
+
+    if (freind == 2)
+    //else
+    {
+        time = distance / (dogSpeed + secondFreindSpeed);
+        freind = 1;
+    }
+
+    distance = distance - (firstFreindSpeed + secondFreindSpeed) * time;
+    count = count + 1;
+}
+
+Console.WriteLine(count);
