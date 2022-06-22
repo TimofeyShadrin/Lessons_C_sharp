e6d1a729c7fac76e2bcfd737588aba3abb22e82f
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-22 19:04:06 +0300

    Ex 003 sorting by choice continue

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int[] numbers = new int[5];
+
+foreach (int i in numbers)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+foreach (int j in numbers)
+{
+    Console.Write($"{numbers[j]} \t");
+}
