2ce732107f17e6930a99683bd87452d9c00479c2
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-23 22:37:17 +0300

    Work on seminar 1 from C#

---
+++ Sem001/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld 
+{ class Program 
+    { static void Main(string[] args) 
+        { 
+            Console.WriteLine("Input value: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //Получаем число
+            int square = Convert.ToInt32(Math.Pow(number, 2));
+            // Или получаем и записваем число
+            number *= number;
+            // Вывод результата
+            Console.WriteLine($"Square is: {square}");
+
+        } 
+    } 
+} 
+
+// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго. 
+
+// 2. Напишите программу, которая будет выдавать название дня недели
+//  по заданному номеру.
