8a58ace97279beb9150d99157d15c03f9578e3df
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-25 14:42:01 +0300

    Fixed FillArray and PrintArray

--- L2_find2/Program.cs
+++ L2_find2/Program.cs
@@ -1,26 +1,31 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray(int[] collection) // метод для заполнения массива
 {
     int length = collection.Length;
     int index = 0;
 
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 10);
+        //collection[index] = new Random().Next(1, 10);
+        collection[index] = Convert.ToInt32(Console.ReadLine());
         index++;
     }
 
 }
 
-void PrintArray(int[] col)
+void PrintArray(int[] col) // выводим массив в консоль в виде [1, 2, 3, 4, 5, 6, 7, 8, 9]
 {
     int count = col.Length;
     int position = 0;
+    
+    Console.Write($"[{col[0]},");
 
-    while (position < count)
+    while (position + 1 < count - 1)
     {
-        Console.WriteLine(col[position]);
+        Console.Write($" {col[position]},");
         position++;
     }
+
+    Console.WriteLine($" {col[position]}]");
 }
 
 int indexOf (int[] collection, int find)
@@ -44,10 +49,13 @@ return position;
 
 int[] array = new int[10]; // конструкция обозначает дословно - создай новый массив в котором будет 10 элементов
 
+Console.Clear();
+
 FillArray(array);
 
-array[4] = 4;
-array[7] = 4;
+Console.Clear();
+//array[4] = 4;
+//array[7] = 4;
 
 PrintArray(array);
 
