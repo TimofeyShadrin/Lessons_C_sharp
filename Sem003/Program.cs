7208b69e35a3373a9f773e5b3b2bd369b68aa0f5
Timofei Shadrin <tshadrin@yandex.ru>
2022-07-01 15:29:45 +0300

    Some metods added

--- Sem003/Program.cs
+++ Sem003/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+            // причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+            // Задача 2. Напишите программу, которая по заданному номеру четверти, 
+            // показывает диапазон возможных координат точек в этой четверти (x и y). 
+            
+            // x = Convert.ToInt32(str[i].ToString());
+        
+            // int num1 = Int32.MinValue;
+            // int num2 = Int32.MaxValue;
+            // Console.ReadKey();
+
+            // string[] nums = str.Split(' '); - создать массив из цифр введенных через к примеру пробел.
+
+            // Напишите программу, которая принимает на вход число (N) и 
+            // выдаёт таблицу квадратов чисел от 1 до N. 5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+            
+
+            int x = Convert.ToInt32(Console.ReadLine().ToString());
+            int y = Convert.ToInt32(Console.ReadLine().ToString());
+
+            if (x > 0 && y > 0)
+            {
+
+            }
+            
+
+
+
+        }
+    }
+}
