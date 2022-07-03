5fc621fcaf9352f3eeb22ccd0e58be494fd37588
Timofei Shadrin <tshadrin@yandex.ru>
2022-07-03 21:58:49 +0300

    Method Sum fixed

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -2,26 +2,33 @@
 {
     class Program
     {
-        static void PrintArrayInt(int[] array, int i)
+        static void PrintArrayInt(int[] array, int i = 0)
         {
-            Console.Write($"{array[i]} ");
-            if (i >= array.Length - 1) return;
-            else
+            if (i < array.Length)
             {
-                i++;
-                PrintArrayInt(array, i);
+                Console.Write($"{array[i]} ");
+                PrintArrayInt(array, i + 1);
             }
         }
 
-        static int Sum(int[] array, int i)
+        // static int Sum(int[] array, int i = 0)
+        // {
+        //     int sum = array[i];
+        //     i++;
+        //     if (i < array.Length) return sum + Sum(array, i);
+        //     else return sum;
+        // }
+        static int Sum(int[] array, int i = 0)
         {
-            int sum = array[i];
-            i++;
-            if (i < array.Length) return sum + Sum(array, i);
-            else return sum;
+            if (i >= array.Length) return 0;
+            int result = Sum(array, i + 1); // рекурсия сначала создает стэки с заполненными функциями,
+                                            // а потом при доставании их тз него делает действия в обратном порядка,
+                                            // начиная с i = array.Length - 1
+            return result + array[i];
         }
 
-        static int Str(string text, int i)
+
+        static int Str(string text, int i = 0)
         {
             int sum = Convert.ToInt32(text[i].ToString());
             i++;
@@ -33,11 +40,11 @@
         {
 
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
-            PrintArrayInt(numbers, 0);
+            PrintArrayInt(numbers);
             Console.WriteLine();
-            Console.WriteLine(Sum(numbers, 0));
+            Console.WriteLine(Sum(numbers));
             string? input = Console.ReadLine();
-            if (!string.IsNullOrEmpty(input)) Console.WriteLine(Str(input, 0));
+            if (!string.IsNullOrEmpty(input)) Console.WriteLine(Str(input));
 
         }
 
