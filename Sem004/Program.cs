009d8fe4d79a4d5f96bf57b02ed559b432c951d0
Timofei Shadrin <tshadrin@yandex.ru>
2022-07-05 14:03:54 +0300

    Examples from Sem 4 added

--- Sem004/Program.cs
+++ Sem004/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace HelloWorld 
-{ class Program 
-    { static void Main(string[] args) 
-        { 
-            
-            
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-        } 
-    } 
-} 
+            // int result = string.Compare(str1, str2);
+            // string reverseText = new string(charArray);
+            // string s = Console.ReadLine();
+            // int[] digits = s.Split(' '); - можно сразу создать массив цифр, а не массив строк.
+            // int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            // 1.Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+            // 4-> 24
+            // 5-> 120
+
+            // 2. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+            // [1,0,1,1,0,1,0,0]
+
+
+
+        }
+    }
+}
